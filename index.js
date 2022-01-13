e6b0720361d7f0f088d175cc04f777cf4b4c9aa9
thiagogvasc <62908111+thiagogvasc@users.noreply.github.com>
2022-01-13 16:31:22 -0500

    Add server

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const http = require('http')
+const path = require('path')
+const { Server } = require('socket.io')
+
+const app = express()
+const server = http.createServer(app)
+const io = new Server(server)
+
+const PORT = 8080
+
+app.use(express.static(path.join(__dirname, '/client/build')));
+
+
+app.get('/', (req, res) => {
+    console.log('page requested')
+    res.sendFile(__dirname + '/client/build/index.html')
+})
+
+
+
+
+server.listen(8080, () => {
+    console.log(`Server listening on port ${PORT}`)
+})
