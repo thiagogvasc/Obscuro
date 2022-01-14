fc79ceefd802b91c40984ac364acec16e05df576
thiagogvasc <62908111+thiagogvasc@users.noreply.github.com>
2022-01-14 13:27:12 -0500

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express()
 const server = http.createServer(app)
 const io = new Server(server)
 
-const PORT = 8080
+const PORT = process.env.PORT || 8080
 
 app.use(express.static(path.join(__dirname, '/client/build')));
 
