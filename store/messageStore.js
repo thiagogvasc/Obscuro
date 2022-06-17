d50cd4d024ccb93df27cdbc079e69b7b4783fe20
thiagogvasc <62908111+thiagogvasc@users.noreply.github.com>
2022-06-17 16:56:49 -0400

    Update messages store

--- store/messageStore.js
+++ store/messageStore.js
@@ -1,36 +1,57 @@
+const { uuid } = require('uuidv4')
+
 const messages = []
 
-const addMessage = message => {
-  messages.push(message)
+// const addMessage = message => {
+//   messages.push(message)
+// }
+
+const createMessage = message => {
+  const {senderID, conversationID, text} = message
+  messages.push({
+    id: uuid(),
+    senderID,
+    conversationID,
+    text
+  })
 }
 
-const getAllMessagesTo = id => {
-  const myMessages = messages.filter(message => {
-    return message.receiver.id === id || message.receiver.isRoom || (message.sender.id === id && !message.receiver.isRoom)
-  })
+const getAllMessagesByConversationID = conversationID => {
+  messages.filter(message => message.conversationID === conversationID)
+}
 
-  myMessagesObject = {}
-  myMessages.forEach(message => {
-    if (message.receiver.isRoom) {
-      if (!myMessagesObject[message.receiver.id])
-        myMessagesObject[message.receiver.id] = []
+// const getAllMessagesTo = id => {
+//   const myMessages = messages.filter(message => {
+//     return message.receiver.id === id || message.receiver.isRoom || (message.sender.id === id && !message.receiver.isRoom)
+//   })
 
-      myMessagesObject[message.receiver.id].push(message)
-    } else {
-      if (!myMessagesObject[message.sender.id])
-        myMessagesObject[message.sender.id] = []
+//   myMessagesObject = {}
+//   myMessages.forEach(message => {
+//     if (message.receiver.isRoom) {
+//       if (!myMessagesObject[message.receiver.id])
+//         myMessagesObject[message.receiver.id] = []
 
-      if (!myMessagesObject[message.receiver.id])
-        myMessagesObject[message.receiver.id] = []
+//       myMessagesObject[message.receiver.id].push(message)
+//     } else {
+//       if (!myMessagesObject[message.sender.id])
+//         myMessagesObject[message.sender.id] = []
+
+//       if (!myMessagesObject[message.receiver.id])
+//         myMessagesObject[message.receiver.id] = []
         
-      myMessagesObject[message.sender.id].push(message)
+//       myMessagesObject[message.sender.id].push(message)
 
-      if (message.sender.id === id)
-        myMessagesObject[message.receiver.id].push(message)
-    }
-  })
-  return myMessagesObject
+//       if (message.sender.id === id)
+//         myMessagesObject[message.receiver.id].push(message)
+//     }
+//   })
+//   return myMessagesObject
+// }
+
+
+// module.exports = { addMessage, getAllMessagesTo }
+
+module.exports = {
+  createMessage,
+  getAllMessagesByConversationID
 }
-
-
-module.exports = { addMessage, getAllMessagesTo }
