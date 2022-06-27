53fe23e0c6ae0e5147f61a47726f207e3c00a246
thiagogvasc <62908111+thiagogvasc@users.noreply.github.com>
2022-06-27 14:04:11 -0400

    add auth controller

---
+++ server/src/controllers/authController.js
@@ -0,0 +1,33 @@
+const userService = require('../service/userService')
+
+
+const login = async (req, res) => {
+  const { username, password } = req.body
+  const user = await userService.getUserByUsername(username)
+
+  // return if username not found
+  if (!user) return res.send('username not found')
+
+  if (user.password === password) {
+    res.send('logged in')
+  } else {
+    res.send('incorrect password')
+  }
+}
+
+const signup = async (req, res) => {
+  const { username, password } = req.body
+  const existingUser = await userService.getUserByUsername(username)
+
+  // Username already exists
+  if (existingUser) return res.send('user already exists')
+
+  // Create new user
+  const newUser = await userService.createUser(username, password)
+  res.send('user created')
+}
+
+module.exports = {
+  login,
+  signup
+}
