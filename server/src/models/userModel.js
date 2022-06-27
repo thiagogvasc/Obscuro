0aa08ebf7c25c58bf3c3f90e1e265e1e092e6b0c
thiagogvasc <62908111+thiagogvasc@users.noreply.github.com>
2022-06-27 14:06:09 -0400

    Add user password field

--- server/src/models/userModel.js
+++ server/src/models/userModel.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose')
 const { Schema } = mongoose
 
 const userSchema = new Schema({
-    _id: String,
-    socket: String,
-    username: String,
-    conversations: [{ type: String }]
+    _id: { type: String, required: true},
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true},
+    conversations: [{ type: String, required: true }]
 })
 
 module.exports = mongoose.model('user', userSchema)
