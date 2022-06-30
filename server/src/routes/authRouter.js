cff907fff43cf0d200a9484c02dda68077a52bca
thiagogvasc <62908111+thiagogvasc@users.noreply.github.com>
2022-06-30 09:41:34 -0400

    Change get to post

--- server/src/routes/authRouter.js
+++ server/src/routes/authRouter.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const authController = require('../controllers/authController')
 
-router.get('/login', authController.login)
-router.get('/signup', authController.signup)
+router.post('/login', authController.login)
+router.post('/signup', authController.signup)
 
 module.exports = router
