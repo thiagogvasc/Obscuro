d2455dde317ae95a9b4f740dcbbfc0bc9b01728c
thiagogvasc <62908111+thiagogvasc@users.noreply.github.com>
2022-07-04 20:13:23 -0400

    Add send general message handler

--- server/src/eventHandlers/messageHandler.js
+++ server/src/eventHandlers/messageHandler.js
@@ -4,9 +4,16 @@ const messageService = require('../service/messageService')
 const sendMessage = async (socket, io, message) => {
   const newMessage = await messageService.createMessage(message.text, message.sender, message.conversation)
   const aggregateMessage = await messageService.getAggregateMessageById(newMessage._id)
-  io.to(message.conversation).to(message.sender).emit('message', aggregateMessage)
+  io.to(message.conversation).emit('message', aggregateMessage)
+}
+
+// Sends message to the General chat
+// messages in the General chat are not persistent
+const sendGeneralMessage = async (socket, io, message) => {
+  io.to('General', message)
 }
 
 module.exports = {
-  sendMessage
+  sendMessage,
+  sendGeneralMessage
 }
