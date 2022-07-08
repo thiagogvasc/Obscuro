2927cd22275826b29f5ce6c14e68d83ea5b9692c
thiagogvasc <62908111+thiagogvasc@users.noreply.github.com>
2022-07-07 21:04:06 -0400

    Fix join room on connect

--- server/src/eventHandlers/conversationHandler.js
+++ server/src/eventHandlers/conversationHandler.js
@@ -3,11 +3,9 @@ const userService = require('../service/userService')
 
 
 const createConversation = async (socket, io, {name, isPublic, isDM}) => {
-  const newConversation = await conversationService.createConversation(name, isPublic, isDM, [])
-  const creator = await userService.getUserById(socket.request.session.userid)
-  
-  await conversationService.addParticipantToConversationById(newConversation._id, creator._id)
-  await userService.addConversationToUserById(creator._id, newConversation._id)
+  const newConversation = await conversationService.createConversation(name, isPublic, isDM, []) 
+  await joinConversation(socket, io, newConversation._id)
+  // emit conversation created event with aggregate info
 }
 
 const deleteConversation = async (socket, io) => {
@@ -23,8 +21,8 @@ const joinChat = async (socket, io) => {
   const userID = socket.request.session.userid
   console.log(userID)
   const generalConv = await conversationService.getConversationByName('General')
-  await joinConversation(socket, io, generalConv._id)
-  socket.join(generalConv._id)
+  if (!generalConv.participants.includes(userID))
+    await joinConversation(socket, io, generalConv._id)
   const aggregateUser = await userService.getAggregateUserById(userID)
   socket.emit('chat-joined', aggregateUser)
 }
@@ -35,6 +33,7 @@ const joinConversation = async (socket, io, conversationID) => {
   const userID = socket.request.session.userid 
   await conversationService.addParticipantToConversationById(conversationID, userID)
   await userService.addConversationToUserById(userID, conversationID)
+  await socket.join(conversationID)
   socket.emit('conversation-joined') // io.emit(new user joined)
   // emit 10 last messages
 }
--- server/src/io.js
+++ server/src/io.js
@@ -4,14 +4,35 @@ const registerMessageEvents = require('./events/messageEvents')
 const registerUserEvents = require('./events/userEvents')
 const sessionMiddleware = require('./middleware/sessionMiddleware')
 
+//FIX
+const userService = require('./service/userService')
+
 
 let io = null
 const init = (server, options) => {
   io = new Server(server, options)
   const wrap = middleware => (socket, next) => middleware(socket.request, {}, next)
   io.use(wrap(sessionMiddleware))
+  // io.of("/").adapter.on("join-room", (room, id) => {
+  //   console.log(`socket ${id} has joined room ${room}`)
+  // })
+  io.use((socket, next) => {
+    const session = socket.request.session;
+    if (session && session.userid) {
+      next()
+    } else {
+      next(new Error("unauthorized"));
+    }
+  })
   io.on('connection', socket => {
     console.log('connectin: ', socket.request.session)
+    const userID = socket.request.session.userid
+    // handle session if (!session) res.redirect()
+    // join all rooms on reconnect
+    // prevents lost rooms on server restart
+    userService.getUserById(userID).then(user => {
+      user.conversations.forEach(conversation => socket.join(conversation))
+    })
     registerUserEvents(socket, io)
     registerMessageEvents(socket, io)
     registerConversationEvents(socket, io)
