19ce3fa520b3216bc71328603593166999bbaad1
thiagogvasc <62908111+thiagogvasc@users.noreply.github.com>
2022-07-08 11:31:25 -0400

    Link user router

--- server/src/app.js
+++ server/src/app.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 const conversationService = require('./service/conversationService')
 const dbConnect = require('./dbConnect')
 const authRouter = require('./routes/authRouter')
+const userRouter = require('./routes/userRouter')
 const sessionMiddleware = require('./middleware/sessionMiddleware')
 
 const app = express()
@@ -23,6 +24,7 @@ const init = async () => {
     app.use(bodyParser.json())
     app.use(sessionMiddleware)
     app.use('/auth', authRouter)
+    app.use('/user', userRouter)
 }
 
 init()
