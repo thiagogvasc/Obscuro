61b78d573a38ecab4cfa103a93e11137e19d40c8
thiagogvasc <62908111+thiagogvasc@users.noreply.github.com>
2022-07-19 21:23:41 -0400

    Add unread notifications

--- client/src/components/SidebarChat.js
+++ client/src/components/SidebarChat.js
@@ -1,14 +1,28 @@
 import React from 'react'
 import Box from '@mui/material/Box'
 import Paper from '@mui/material/Paper'
-import { Avatar, Typography } from '@mui/material'
+import { Avatar, Typography, Badge } from '@mui/material'
 import { grey } from '@mui/material/colors'
 import { useChat } from '../contexts/chatContext'
+import { useUser } from '../contexts/userContext'
+import { useSocket } from '../contexts/socketContext'
 
 export default function SidebarChat({ conversation, shouldOpenSidebar, setShouldOpenSidebar}) {
   const { chatData, currentConversation, setCurrentConversation } = useChat()
-
+  const { userID } = useUser()
   const shouldHighlight = conversation._id === currentConversation._id
+  const socket = useSocket()
+
+  const getNumberOfUnreadMessages = () => {
+    let count = 0
+    conversation.messages.forEach(message => {
+      if (message.conversation._id === currentConversation._id) return
+      if (!message.readBy.includes(userID)) count++
+    })
+    return count
+  }
+
+  const numberOfUnreadMessages = getNumberOfUnreadMessages()
 
   const getConvName = () => {
     if (conversation.isDM) {
@@ -52,6 +66,15 @@ export default function SidebarChat({ conversation, shouldOpenSidebar, setShould
       <Box sx={{ alignSelf: 'center', fontWeight:'100' }}>
         <Typography variant="body1" sx={{ }}>{getConvName()}</Typography>
         <Typography variant="body2" sx={{ color: 'darkgray'}}>{getLastMessage()?.text.length > 10 ? getLastMessage()?.text.substring(1, 10) + '...' : getLastMessage()?.text}</Typography>
+      </Box>
+      <Box sx={{ pr: 3, flexGrow: 1, display: 'flex', justifyContent: 'end', alignItems: 'center'}}>
+      {numberOfUnreadMessages ? 
+        <Box sx={{ width: '25px', height: '25px', borderRadius: '50%', backgroundColor: 'primary.main', textAlign: 'center'}}>
+          <Typography variant="body1">{numberOfUnreadMessages}</Typography>
+        </Box>
+        : 
+        null
+      }
       </Box>
     </Paper>
   )
