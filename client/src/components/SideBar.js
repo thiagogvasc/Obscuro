ed1055f0e96a5d30c047db8577878dde01d366a7
thiagogvasc <62908111+thiagogvasc@users.noreply.github.com>
2022-07-24 23:07:36 -0400

    Add slide transition

--- client/src/components/SideBar.js
+++ client/src/components/SideBar.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 import Paper from '@mui/material/Paper'
@@ -8,10 +8,12 @@ import Tabs from '@mui/material/Tabs'
 import Box from '@mui/material/Box'
 import AddIcon from '@mui/icons-material/Add'
 import Fab from '@mui/material/Fab'
+import Slide from '@mui/material/Slide'
 
 import SidebarChat from './SidebarChat'
 import SidebarUser from './SidebarUser'
 import { Typography } from '@mui/material'
+import { TransitionGroup } from 'react-transition-group';
 
 import { useChat } from '../contexts/chatContext'
 import { useUsers } from '../contexts/usersContext'
@@ -21,11 +23,12 @@ export default function Sidebar({ shouldOpenSidebar, setShouldOpenSidebar }) {
   const { chatData, currentConversation } = useChat()
   const { users } = useUsers()
   const [tab, setTab] = useState('conversations')
+  const containerRef = useRef(null)
   const handleChange = (e, newValue) => {
     setTab(newValue)
   }
   return (
-    <Paper elevation={2} sx={{ 
+    <Paper ref={containerRef} elevation={2} sx={{ 
       display: 'flex',
       flexDirection: 'column',
       display: {
@@ -35,50 +38,55 @@ export default function Sidebar({ shouldOpenSidebar, setShouldOpenSidebar }) {
       // backgroundColor: grey[800],
       width: shouldOpenSidebar ? '100%' : '25%', 
       borderRadius: '25px',
-      minWidth: '250px'
+      minWidth: '250px',
+      overflow: 'hidden'
     }}>
       
 
-      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+      <Paper elevation={3} sx={{ borderBottom: 1, borderColor: 'divider' }}>
         <Tabs variant="fullWidth" value={tab} onChange={handleChange}>
           <Tab label="Conversations" value="conversations" />
           <Tab label="Users" value="users" />
         </Tabs>
-      </Box>
-      
+      </Paper>
+
       { tab === 'conversations' ? 
-      <Box sx={{
-        flexGrow: 1,
-        display:'flex',
-        flexDirection:'column'
-      }}>
-        <Box sx={{
-          overflowY: 'scroll',
-          flexGrow: 1,
-          height: 0
-        }}>
-          
-          <Typography fontWeight="light" ml={3} mt={1} /*color="white"*/ variant="h5">Public</Typography>
-          {chatData.conversations.filter(conversation => conversation.isPublic).map(conversation => <SidebarChat key={conversation._id} shouldOpenSidebar={shouldOpenSidebar} setShouldOpenSidebar={setShouldOpenSidebar} conversation={conversation} />)}  
-          
-          <Typography fontWeight="light" ml={3} mt={1} /*color="white"*/ variant="h5">Private</Typography>
-          {chatData.conversations.filter(conversation => !conversation.isPublic).map(conversation => <SidebarChat key={conversation._id} shouldOpenSidebar={shouldOpenSidebar} setShouldOpenSidebar={setShouldOpenSidebar} conversation={conversation} />)}
-       
-        </Box>
-        
-        <Box sx={{ p: 1, textAlign: 'center' }}>
-          <Typography mt={1} variant="body1" fontWeight="light">Create</Typography>
-          <Fab color="primary" variant="contained" onClick={() => navigate('/chat/create-conversation')}>
-            <AddIcon />
-          </Fab>
-        </Box>
-      </Box>
-      
-      :
-      <>
-        <Typography fontWeight="light" ml={3} mt={1} color="white" variant="h5">Users</Typography>
-        {users.map(user => <SidebarUser key={user._id} setTab={setTab} shouldOpenSidebar={shouldOpenSidebar} setShouldOpenSidebar={setShouldOpenSidebar} user={user} />)}  
-      </>
+        <Slide in direction="right" container={containerRef.current}>
+          <Box sx={{
+            flexGrow: 1,
+            display:'flex',
+            flexDirection:'column'
+          }}>
+            <Box sx={{
+              overflowY: 'scroll',
+              flexGrow: 1,
+              height: 0
+            }}>
+              
+              <Typography fontWeight="light" ml={3} mt={1} /*color="white"*/ variant="h5">Public</Typography>
+              {chatData.conversations.filter(conversation => conversation.isPublic).map(conversation => <SidebarChat key={conversation._id} shouldOpenSidebar={shouldOpenSidebar} setShouldOpenSidebar={setShouldOpenSidebar} conversation={conversation} />)}  
+              
+              <Typography fontWeight="light" ml={3} mt={1} /*color="white"*/ variant="h5">Private</Typography>
+              {chatData.conversations.filter(conversation => !conversation.isPublic).map(conversation => <SidebarChat key={conversation._id} shouldOpenSidebar={shouldOpenSidebar} setShouldOpenSidebar={setShouldOpenSidebar} conversation={conversation} />)}
+           
+            </Box>
+            <Paper elevation={3} sx={{ p: 1, textAlign: 'center' }}>
+              <Typography mt={1} variant="body1" fontWeight="light">Create</Typography>
+              <Fab color="primary" variant="contained" onClick={() => navigate('/chat/create-conversation')}>
+                <AddIcon />
+              </Fab>
+            </Paper>
+          </Box>
+        </Slide>
+      : 
+        <>
+          <Slide in={tab==='users'} direction="left" container={containerRef.current}>
+            <Box sx={{ }}>
+              <Typography fontWeight="light" ml={3} mt={1} color="white" variant="h5">Users</Typography>
+              {users.map(user => <SidebarUser key={user._id} setTab={setTab} shouldOpenSidebar={shouldOpenSidebar} setShouldOpenSidebar={setShouldOpenSidebar} user={user} />)}  
+            </Box>
+          </Slide>
+        </>
       }
 
     </Paper>
