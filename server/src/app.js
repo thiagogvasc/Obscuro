e981d71d0c039b807495b7c374eca25ee63876aa
thiagogvasc <62908111+thiagogvasc@users.noreply.github.com>
2022-07-29 13:45:56 -0400

    Edit static files path

--- server/src/app.js
+++ server/src/app.js
@@ -23,9 +23,9 @@ const init = async () => {
     }))
     app.use(bodyParser.urlencoded({ extended: false }))
     app.use(bodyParser.json())
-    //app.use(express.static(''))
+    app.use(express.static(path.join(__dirname, '../../client/build')))
     app.get('/', (req, res) => {
-      res.sendFile(path.join(__dirname, '../../client/build/index.html'))
+      res.sendFile(path.join(__dirname, 'index.html'))
     })
     app.use(sessionMiddleware)
     app.use('/auth', authRouter)
