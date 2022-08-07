28079c435687c10255bb9cdff92c4c1d382aa84e
thiagogvasc <62908111+thiagogvasc@users.noreply.github.com>
2022-08-07 16:35:18 -0400

    Add conversation dropdown

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -9,14 +9,21 @@ import Box from '@mui/material/Box'
 import AddIcon from '@mui/icons-material/Add'
 import Fab from '@mui/material/Fab'
 import Slide from '@mui/material/Slide'
+import Collapse from '@mui/material/Collapse'
 
 import SidebarChat from './SidebarChat'
 import SidebarUser from './SidebarUser'
 import { Typography } from '@mui/material'
 import { TransitionGroup } from 'react-transition-group';
 
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
 import { useChat } from '../contexts/chatContext'
 import { useUsers } from '../contexts/usersContext'
+import { grey } from '@mui/material/colors'
 
 export default function Sidebar({ shouldOpenSidebar, setShouldOpenSidebar }) {
   const navigate = useNavigate()
@@ -27,6 +34,17 @@ export default function Sidebar({ shouldOpenSidebar, setShouldOpenSidebar }) {
   const handleChange = (e, newValue) => {
     setTab(newValue)
   }
+
+  const publicConversations = []
+  const privateConversations = []
+
+  for (const conversation of chatData.conversations) {
+    conversation.isPublic ? 
+      publicConversations.push(conversation) :
+      privateConversations.push(conversation) 
+  }
+
+
   return (
     <Paper ref={containerRef} elevation={2} sx={{ 
       display: 'flex',
@@ -51,7 +69,7 @@ export default function Sidebar({ shouldOpenSidebar, setShouldOpenSidebar }) {
       </Paper>
 
       { tab === 'conversations' ? 
-        <Slide in direction="right" container={containerRef.current}>
+        <Slide in={tab === 'conversations'} appear mountOnEnter unmountOnExit direction="right" container={containerRef.current}>
           <Box sx={{
             flexGrow: 1,
             display:'flex',
@@ -60,14 +78,28 @@ export default function Sidebar({ shouldOpenSidebar, setShouldOpenSidebar }) {
             <Box sx={{
               overflowY: 'scroll',
               flexGrow: 1,
-              height: 0
+              height: 0 
             }}>
+
+              <Accordion>
+                <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                <Typography fontWeight="light" ml={3} mt={1} variant="h5">Public</Typography>
+                </AccordionSummary>
+                <AccordionDetails sx={{padding: 0}}>
+                  {publicConversations.map(conversation => <SidebarChat key={conversation._id} shouldOpenSidebar={shouldOpenSidebar} setShouldOpenSidebar={setShouldOpenSidebar} conversation={conversation} />)}  
+                  {publicConversations.length === 0 && <Typography>No public conversations yet</Typography>}
+                </AccordionDetails>
+              </Accordion>
               
-              <Typography fontWeight="light" ml={3} mt={1} /*color="white"*/ variant="h5">Public</Typography>
-              {chatData.conversations.filter(conversation => conversation.isPublic).map(conversation => <SidebarChat key={conversation._id} shouldOpenSidebar={shouldOpenSidebar} setShouldOpenSidebar={setShouldOpenSidebar} conversation={conversation} />)}  
-              
-              <Typography fontWeight="light" ml={3} mt={1} /*color="white"*/ variant="h5">Private</Typography>
-              {chatData.conversations.filter(conversation => !conversation.isPublic).map(conversation => <SidebarChat key={conversation._id} shouldOpenSidebar={shouldOpenSidebar} setShouldOpenSidebar={setShouldOpenSidebar} conversation={conversation} />)}
+              <Accordion>
+                <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                <Typography fontWeight="light" ml={3} mt={1} variant="h5">Private</Typography>
+                </AccordionSummary>
+                <AccordionDetails sx={{padding: 0}}>
+                  {privateConversations.map(conversation => <SidebarChat key={conversation._id} shouldOpenSidebar={shouldOpenSidebar} setShouldOpenSidebar={setShouldOpenSidebar} conversation={conversation} />)}
+                  {privateConversations.length === 0 && <Typography textAlign="center">No private conversations yet</Typography>}
+                </AccordionDetails>
+              </Accordion>
            
             </Box>
             <Paper elevation={3} sx={{ p: 1, textAlign: 'center' }}>
@@ -78,15 +110,14 @@ export default function Sidebar({ shouldOpenSidebar, setShouldOpenSidebar }) {
             </Paper>
           </Box>
         </Slide>
-      : 
-        <Slide in={tab==='users'} direction="left" container={containerRef.current}>
+          :
+        <Slide in={tab === 'users'} appear mountOnEnter unmountOnExit direction="left" orientation="horizontal" container={containerRef.current}>
           <Box sx={{ flexGrow: 1, overflowY: 'scroll', height: 0 }}>
             <Typography fontWeight="light" ml={3} mt={1} color="white" variant="h5">Users</Typography>
             {users.map(user => <SidebarUser key={user._id} setTab={setTab} shouldOpenSidebar={shouldOpenSidebar} setShouldOpenSidebar={setShouldOpenSidebar} user={user} />)}  
           </Box>
         </Slide>
       }
-
     </Paper>
   )
 }
